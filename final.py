5b556fd1e6d20b73cad5fe8cadf3c4c2b9f55f5a
jolly <jollyc0211@gmail.com>
2021-05-25 14:15:19 +0530

    first commit

---
+++ final.py
@@ -0,0 +1,44 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+
+
+
+
+def takecommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("\nListening.....")
+        r.pause_threshold = 1
+        r.energy_threshold=500
+        audio=r.listen(source)
+
+    try:
+        print('Recognizing....')
+        query=r.recognize_google(audio,language='en-in')
+        print(f'user said: {query}')
+
+    except Exception as e:
+        print('Say that again please...')
+        return 'None'
+
+    return query
+
+
+if __name__=="__main__":
+    
+    takecommand()
+            
